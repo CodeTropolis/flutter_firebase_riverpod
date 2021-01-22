a050abc6e767c016f2d5981d0f1ee62c29960873
Michael D <michael@codetropolis.com>
2021-01-22 09:59:12 -0600

    User list - delete funciton working.

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,19 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_application_1/models/user.dart';
 import 'package:hooks_riverpod/all.dart';
+import 'package:uuid/uuid.dart';
 
 class FirestoreService {
   final firebaseUserProvider = StreamProvider<List<User>>((ref) {
     final stream = FirebaseFirestore.instance.collection('users').snapshots();
     return stream.map((snapshot) => snapshot.docs.map((doc) {
-          // print('doc id: ${doc.id}');
-          return User.fromJson({'id': doc.id, ...doc.data()});
-          // return User.fromJson(doc.data());
+          print('doc data: ${doc.data()}');
+          return User.fromJson(doc.data());
         }).toList());
   });
 
   Future<void> setUser(User user) {
-    return FirebaseFirestore.instance.collection('users').add(user.toMap());
+    // print('setUser: $user');
+    var uuid = new Uuid();
+    var _id = uuid.v4();
+    var _user = new User(id: _id, name: user.name, role: user.role, desc: user.desc);
+    return FirebaseFirestore.instance.collection('users').doc(_id).set(_user.toMap());
   }
 
   Future<void> deleteUser(String userId) {
