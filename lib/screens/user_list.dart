135047ae3954ddaa0ff6fa7b71bc317fe34bb026
Michael D <michael@codetropolis.com>
2021-01-28 07:39:59 -0600

    Add location wip.

--- lib/screens/user_list.dart
+++ lib/screens/user_list.dart
@@ -3,20 +3,23 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/all.dart';
 import 'package:flutter_application_1/screens/user_entry.dart';
+import 'package:location/location.dart';
 
 final firestoreService = FirestoreService();
 
 class UserList extends HookWidget {
+  final Location location = Location();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Users'),
+        title: Text('Location:'),
       ),
       body: useProvider(firestoreService.firebaseUserProvider).when(
           loading: () => const Center(child: CircularProgressIndicator()),
           error: (err, stack) => Center(child: Text(err.toString())),
           data: (users) {
+            print('$location');
             return ListView.separated(
                 separatorBuilder: (c, i) => Divider(color: Colors.black26),
                 itemCount: users.length,
